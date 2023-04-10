198ea474314a90a72cbf4a3a4de1ea24a9975690
Natalia Galicka <natalia.galicka.programista@gmail.com>
2023-04-10 16:32:05 +0200

    Added icons to various weather descriptions. Change in view layout. Added display of icons in the main view. Added weather display for the next four days.

--- src/main/java/weatherapp/controller/CurrentWeatherController.java
+++ src/main/java/weatherapp/controller/CurrentWeatherController.java
@@ -6,14 +6,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import weatherapp.Config;
 import weatherapp.model.WeatherManager;
 import weatherapp.view.ViewFactory;
 
-
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,59 +25,107 @@ import java.util.ResourceBundle;
 
 public class CurrentWeatherController extends BaseController implements Initializable{
 
+	 @FXML
+	    private VBox body;
+
+	    @FXML
+	    private TextField chooseLocation;
+
+	    @FXML
+	    private TextField chooseLocation1;
+
+	    @FXML
+	    private Label currentCity;
+
+	    @FXML
+	    private Label currentCity1;
+
+	    @FXML
+	    private Label currentDate;
+
+	    @FXML
+	    private Label currentDate1;
+
+	    @FXML
+	    private Label currentDegree11;
+
+	    @FXML
+	    private Label currentDegree111;
+
+	    @FXML
+	    private Label currentHumidity;
+
+	    @FXML
+	    private Label currentHumidity1;
+
+	    @FXML
+	    private ImageView currentPicture1;
+
+	    @FXML
+	    private ImageView currentPicture11;
+
+	    @FXML
+	    private Label currentPressure2;
+
+	    @FXML
+	    private Label currentPressure21;
+
+	    @FXML
+	    private Label currentWindSpeed;
+
+	    @FXML
+	    private Label currentWindSpeed1;
+
+	    @FXML
+	    private Label description;
+
+	    @FXML
+	    private Label description1;
+
+	    @FXML
+	    private VBox hboxNew;
+
+	    @FXML
+	    private VBox hboxNew1;
+
+	    @FXML
+	    private Label sensedTemperature;
+
+	    @FXML
+	    private Label sensedTemperature1;
+
+	    @FXML
+	    private HBox weatherNextDay;
+	    
+	    @FXML
+	    private HBox hboxNextDayWeather;
+
+	    @FXML
+	    private HBox hboxNextDayWeather1;
+	    
+	    @FXML
+	    private VBox vbox1;
+
+	    @FXML
+	    private VBox vbox2;
+
+	    @FXML
+	    private VBox vbox3;
+
+	    @FXML
+	    private VBox vbox4;
 
     
-    @FXML
-    private VBox body;
-
-    @FXML
-    private TextField chooseLocation;
-
-    @FXML
-    private ListView<?> cityList;
-
-    @FXML
-    private Label currentCity;
-
-    @FXML
-    private Label currentDate;
-
-    @FXML
-    private Label currentDegree11;
-
-    @FXML
-    private Label currentHumidity;
-
-    @FXML
-    private ImageView currentPicture1;
-
-    @FXML
-    private Label currentPressure2;
-
-    @FXML
-    private Label currentTime;
-
-    @FXML
-    private Label currentWindSpeed;
-
-    @FXML
-    private Label description;
-
-    @FXML
-    private Label sensedTemperature;
-
-    @FXML
-    private HBox weatherNextDay;
-
-    
-    String[] cities = {"Izabelin","Truskaw","Warszawa","Londyn"};
-    
-    String currentLocation;
+	    public CurrentWeatherController(WeatherManager weatherManager, ViewFactory viewFactory, String fxmlName) {
+	        super(weatherManager, viewFactory, fxmlName);
+	    }
     
     @FXML
     void getLocation(ActionEvent event) throws IOException {
     	
-    	String cityName = "Truskaw";
+    	hboxNextDayWeather.getChildren().clear();
+    	
+    	String cityName = "Londyn";
     	
     	String YOUR_API_KEY = Config.API_KEY;
     	
@@ -89,51 +140,17 @@ public class CurrentWeatherController extends BaseController implements Initiali
     	
 
     	weatherManager.getWeatherData(chooseLocation, currentCity, currentDate,
-    			currentDegree11,currentHumidity, currentPicture1, currentPressure2, currentTime, currentWindSpeed, description,
-sensedTemperature, weatherNextDay);
+    			currentDegree11,currentHumidity, currentPicture1, currentPressure2, currentWindSpeed, description,
+sensedTemperature, weatherNextDay, hboxNextDayWeather);
      	
     }    
     
 
-    public CurrentWeatherController(WeatherManager weatherManager, ViewFactory viewFactory, String fxmlName) {
-        super(weatherManager, viewFactory, fxmlName);
-    }
-
-
-
-
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		// TODO Auto-generated method stub
-		/*
-		cityList.getItems().addAll(cities);
-		
-		cityList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				// TODO Auto-generated method stub
-				currentLocation = cityList.getSelectionModel().getSelectedItem();
-				
-				System.out.println(currentLocation);
-				
-				chooseLocation.setText(currentLocation);
-				
-				WeatherManager weatherManager = new WeatherManager();
-				
-				
-				weatherManager.setCityName(currentLocation);
-				
-		    	
-		    	weatherManager.getWeatherData(chooseLocation, currentCity, currentDate,
-		    			currentDegree11,currentHumidity, currentPicture1, currentPressure2, currentTime, currentWindSpeed, description,
-		sensedTemperature);
-				
-			}	
-			});
-			*/
-		
-		
+/*
+ * W tym miejscu pobrać dane obecnej lokalizacji użytkownika
+ */
 		String cityName = "Izabelin";
     	
     	String YOUR_API_KEY = Config.API_KEY;
@@ -144,13 +161,25 @@ sensedTemperature, weatherNextDay);
     	
     	String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + cityName + "?unitGroup=" + UNIT_GROUP + "&key=" + YOUR_API_KEY + "&contentType=" + CONTENT_TYPE + "&lang=pl";
     	
-		
+    	DropShadow dropShadow = new DropShadow();
+        dropShadow.setBlurType(BlurType.THREE_PASS_BOX);
+        dropShadow.setWidth(40);
+        dropShadow.setHeight(40);
+        dropShadow.setRadius(19.5);
+        dropShadow.setOffsetX(0);
+        dropShadow.setOffsetY(00);
+        dropShadow.setColor(Color.color(0, 0, 0));
+        hboxNew.setEffect(dropShadow);
+        hboxNew1.setEffect(dropShadow);
+        hboxNextDayWeather.setEffect(dropShadow);
+        hboxNextDayWeather1.setEffect(dropShadow);
+        
 		WeatherManager weatherManager = new WeatherManager(url);
     	
+				weatherManager.getWeatherData(chooseLocation, currentCity, currentDate,
+						currentDegree11,currentHumidity, currentPicture1, currentPressure2, currentWindSpeed, description,
+sensedTemperature, weatherNextDay,hboxNextDayWeather);
 
-	    	weatherManager.getWeatherData(chooseLocation, currentCity, currentDate,
-	    			currentDegree11,currentHumidity, currentPicture1, currentPressure2, currentTime, currentWindSpeed, description,
-	sensedTemperature, weatherNextDay);
 	
 	}
 }
--- src/main/java/weatherapp/model/WeatherManager.java
+++ src/main/java/weatherapp/model/WeatherManager.java
@@ -1,96 +1,98 @@
 package weatherapp.model;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import javafx.fxml.FXML;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-import weatherapp.Config;
 
 public class WeatherManager {
 	
-	/*
-	private String cityName = "Truskaw";
-	
-	String YOUR_API_KEY = Config.API_KEY;
-	
-	String UNIT_GROUP = Config.UNIT_GROUP;
-	
-	String CONTENT_TYPE = Config.CONTENT_TYPE;
-	
-	public String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + cityName + "?unitGroup=" + UNIT_GROUP + "&key=" + YOUR_API_KEY + "&contentType=" + CONTENT_TYPE + "&lang=pl";
-	
-	public String  getUrlString() {
-		return url;
-	}
-	*/
 	
 	public String url;
-
 	
+	public WeatherManager(String url) {
+		super();
+		this.url = url;
+	}
+	
+
 	public void getWeatherData(TextField chooseLocation, Label currentCity, Label currentDate,
 			Label currentDegree11, Label currentHumidity, ImageView currentPicture1, 
-			Label currentPressure2, Label currentTime, Label currentWindSpeed, Label description,
-			Label sensedTemperature, HBox weatherNextDay) {
+			Label currentPressure2, Label currentWindSpeed, Label description,
+			Label sensedTemperature, HBox weatherNextDay, HBox nextDayWeatherContainer){
 		
+	
 		APIConnector apiConn = new APIConnector();
 		
 		JSONObject json = apiConn.connectToAPI(url);
 		
-	      System.out.println(json);
-	     // System.out.println(jsonArr);
+	      //System.out.println(json);
+	      //System.out.println(jsonArr);
 	      System.out.println(json.get("resolvedAddress"));
 	      System.out.println(json.get("address"));
-	      //JSONArray values = json.getJSONArray("currentConditions");
-	      JSONObject values = json.getJSONObject("currentConditions");
-	      System.out.println(values);
 	      
-	     
-				
-	      System.out.println(values.get("icon"));
-	      System.out.println(values.get("humidity"));
-	      System.out.println(values.get("datetime"));
-	      System.out.println(values.get("temp"));
-	      System.out.println(values.get("feelslike"));
-	      System.out.println(values.get("pressure"));
-	      System.out.println(values.get("windspeed"));
-	      System.out.println(values.get("conditions"));
-	     // System.out.println(values.get("description"));
+	      JSONObject values = json.getJSONObject("currentConditions");
+	      //System.out.println(values);
+	 
 	      
 	      ZoneId zoneId=ZoneId.of(json.getString("timezone"));
 
+	      FileInputStream weatherIcon = null;
+		
+	      try {
+			weatherIcon = new FileInputStream("D:\\WeatherApp\\src\\main\\resources\\weatherapp\\images\\sunny.png");
+	      } catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Błąd podczas wczytywania obrazka");
+			e.printStackTrace();
+	      }
+	      
+	        Image weatherImage = new Image(weatherIcon);
+	        
+	        ImageView imageView = new ImageView(weatherImage);
+	        imageView.setFitHeight(80);
+	        imageView.setFitWidth(80);
 			
 	      
 	      currentCity.setText(json.get("resolvedAddress").toString());
 	      currentDegree11.setText(values.get("temp").toString());
-	      currentHumidity.setText(values.get("humidity").toString());
-	      currentPressure2.setText(values.get("pressure").toString());
-	      currentWindSpeed.setText(values.get("windspeed").toString());
+	      currentHumidity.setText("Wilgotność: "+ values.get("humidity").toString() + "%");
+	      currentPressure2.setText("Ciśnienie: " + values.get("pressure").toString() + "hPa");
+	      currentWindSpeed.setText("Wiatr: " + values.get("windspeed").toString() + "km/h");
 	      sensedTemperature.setText("Temperatura odczuwalna: " + values.get("feelslike").toString());
 	      description.setText(values.get("conditions").toString());
+	      currentPicture1.setImage(weatherImage);
 	      
 	      
 	      
-	      JSONArray jsonArray = json.getJSONArray("days");
-	      System.out.println(jsonArray);
+	       JSONArray jsonArray = json.getJSONArray("days");
+	       
+	       //System.out.println(jsonArray);
+		   //System.out.println("Date\tMaxTemp\tMinTemp\tPrecip\tSource%n");
 			
-			System.out.println("Date\tMaxTemp\tMinTemp\tPrecip\tSource%n");
+	       nextDayWeatherContainer.getChildren().clear();
 
-			for (int i = 0; i < 5; i++) {
+			
+			for (int i = 0; i < 4; i++) {
+				
+
 				JSONObject dayValue = jsonArray.getJSONObject(i);
 	            
 	            ZonedDateTime datetime=ZonedDateTime.ofInstant(Instant.ofEpochSecond(dayValue.getLong("datetimeEpoch")), zoneId);
@@ -109,63 +111,67 @@ public class WeatherManager {
 		
 	            System.out.println("%s\t%.1f\t%.1f\t%.1f\t%s%n" + " " + datetime.format(DateTimeFormatter.ISO_LOCAL_DATE) + " " + maxT + " " + minT );
 	    		
-	            
-	            HBox nextTemp =  new HBox(15);
-	            
-	          
+	    		HBox nextTemp =  new HBox(15);
+	    		nextTemp.setAlignment(Pos.CENTER);
 	            Label maxTemp = new Label();
-	            maxTemp.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 20));
+	            VBox dayNext = new VBox(10);
+	            
+	            maxTemp.setFont(Font.font(17));
+	            maxTemp.setTextFill(Color.WHITE);
 	            maxTemp.setText(maxT);
 	            
 	            Label minTemp = new Label();
-	            minTemp.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 20));
+	            minTemp.setFont(Font.font(17));
+	            minTemp.setTextFill(Color.GREY);
 	            minTemp.setText(minT);
-	            
 	            
 	           
 		    	nextTemp.getChildren().add(maxTemp);
 		    	nextTemp.getChildren().add(minTemp);
 		    		
-		    	  VBox dayNext = new VBox(15);
-		    	 
-		    	  String dateString = datetime.format(DateTimeFormatter.ISO_LOCAL_DATE);
-		    	  Label date2 = new Label(dateString);
+		    	
+		    	String dateString = datetime.format(DateTimeFormatter.ISO_LOCAL_DATE);
+		        Label date2 = new Label(dateString);
 		    	  
-		    	  date2.setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 20));
+		        date2.setFont(Font.font(17));
+		        date2.setTextFill(Color.WHITE);
+		       
+		        FileInputStream iconNextDay = null;
+				try {
+					iconNextDay = new FileInputStream("D:\\WeatherApp\\src\\main\\resources\\weatherapp\\images\\sunny.png");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        Image imageNextDay = new Image(iconNextDay);
+		        ImageView imageView1 = new ImageView(imageNextDay);
+		        imageView1.setFitHeight(70);
+		        imageView1.setFitWidth(70);
+		    	
+		    	  
+		    	dayNext.getChildren().add(date2);
+		    	dayNext.getChildren().add(imageView1);
+		    	dayNext.getChildren().add(nextTemp);
+		    	
+		    	dayNext.setAlignment(Pos.CENTER);
 		    	  
 		    	  
-		    	  dayNext.getChildren().add(date2);
-		    	//dayNext.getChildren().add(new ImageView("zdjęcie"));
-		    	  dayNext.getChildren().add(nextTemp);
+		    	nextDayWeatherContainer.getChildren().add(dayNext);
 		    	  
+		    	 dayNext.setPrefHeight(243.0);
+		    	 dayNext.setPrefWidth(190.0);
 		    	  
-		    	  weatherNextDay.getChildren().add(dayNext);
-		    	  
-		    	//dayNext.setAlignment(CENTER);
-		    	  dayNext.setPrefHeight(243.0);
-		    	  dayNext.setPrefWidth(190.0);
-		    	  
-		    	  weatherNextDay.setPadding(new Insets(20.0));
 			
 			}
-			
-			
-			
-			//LocalDate customerBirthday = klient.loadBirthdayFromDatabase();
-			  LocalDate dzisiaj = LocalDate.now();
-			  System.out.println(dzisiaj);
+		
+	}
 	
 	
-	}
 
 
-
-
-
-	public WeatherManager(String url) {
-		super();
-		this.url = url;
-	}
+	
+	
+	
 
 
 
