9e7d1b39a08d557e0c95908ef10d7aa76f3dee36
Natalia Galicka <natalia.galicka.programista@gmail.com>
2023-05-27 07:05:04 +0200

    Deleting unused files

--- src/main/java/weatherapp/App.java
+++ src/main/java/weatherapp/App.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import weatherapp.view.ViewFactory;
 
-
 public class App extends Application {
 	
 	public static void main(String[] args) {
